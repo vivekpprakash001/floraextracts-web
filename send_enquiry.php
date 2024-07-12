bc246ae2d42118520fcf11eafc2646a3e1485d41
Vivek P Prakash <vivekpprakash@xeoscript.com>
2024-07-13 03:43:30 +0530

    Edit in the contact html and send_enquiry php

--- send_enquiry.php
+++ send_enquiry.php
@@ -14,8 +14,8 @@ $subject = "Enquiry from " + $name;
 $txt ="Name = ". $name . "\r\n  Email = "
     . $email . "\r\n Message =" . $message;
 
-$headers = "From: noreply@demosite.com" . "\r\n" .
-            "CC: somebodyelse@example.com";
+$headers = 'From: mail@floraextracts.com'       . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
 if($email != NULL) {
     mail($to, $subject, $txt, $headers);
 }
