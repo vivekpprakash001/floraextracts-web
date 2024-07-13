0aa4a6e19245871be20ba775fee28b833ffe25cf
Vivek P Prakash <vivekpprakash@xeoscript.com>
2024-07-14 01:35:40 +0530

    Test         - Test php

--- send_enquiry.php
+++ send_enquiry.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); ?>
+<?php
+
+<?php
+$to = 'sales@floraextracts.us';
+$subject = 'Test Mail';
+$message = 'This is a test message';
+$headers = 'From: sender@example.com';
+
+if(mail($to, $subject, $message, $headers)) {
+    echo "Mail sent successfully.";
+} else {
+    echo "Mail sending failed.";
+}
+?>
+
+
+?>
