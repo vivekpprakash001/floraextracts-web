835d8dfe3b39c5993a5681ce829b1e5ca3df49f8
Vivek P Prakash <vivekpprakash@xeoscript.com>
2024-07-14 02:01:41 +0530

    Test

--- send_enquiry.php
+++ send_enquiry.php
@@ -1,16 +1,20 @@
 <?php
 
 $to = 'sales@floraextracts.us';
-$subject = 'Test Mail';
+$subject = 'Enquiry from website';
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+$bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+$body = join(PHP_EOL, $bodyParagraphs);
+
 $headers = "From: $email";
 
-if(mail($to, $subject, $message, $headers)) {
-    echo "Mail sent successfully.";
+if(mail($to, $subject, $body, $headers)) {
+    echo '<script>alert("Thank you for reaching out to us. We will be in touch shortly.")</script>';
+    header('Location: contact.html');
 } else {
     echo "Mail sending failed.";
 }
