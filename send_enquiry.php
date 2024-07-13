fd38911fea7526435da477447874af20ce5f10c2
Vivek P Prakash <vivekpprakash@xeoscript.com>
2024-07-14 01:05:03 +0530

    Test         - Test php

--- send_enquiry.php
+++ send_enquiry.php
@@ -3,6 +3,13 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-echo "<script>alert(' . $name . ')</script>";
+
+$toEmail = 'sales@floraextracts.us';
+$emailSubject = 'Enquiry from website';
+$headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
+$bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+$body = join(PHP_EOL, $bodyParagraphs);
+
+echo "<script>alert(' .$body. ')</script>";
 
 ?>
